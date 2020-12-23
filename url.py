348f93000022d3c78c6f38e988013769eef6ca0e
Ramalingasamy M K <69101768+Ramalingasamy012@users.noreply.github.com>
2020-12-23 08:14:41 +0530

    Update url.py

--- url.py
+++ url.py
@@ -5,14 +5,14 @@ def urlinfo():
     print('')
     print('url >> '+inurl)
     url = 'https://www.virustotal.com/vtapi/v2/url/scan'
-    params = {'apikey': '28fadf8f17d413060698031cee5ce3d53cae7be223fe01c11b0203af922b941e', 'url':inurl}
+    params = {'apikey': 'APIKEY', 'url':inurl}
     response = requests.post(url, data=params)
     dictres=response.json()
     print('')
     scanid=dictres['scan_id']
     print("[+] Checking URl......")
     scanurl = 'https://www.virustotal.com/vtapi/v2/url/report'
-    params = {'apikey': '28fadf8f17d413060698031cee5ce3d53cae7be223fe01c11b0203af922b941e', 'resource': scanid }
+    params = {'apikey': 'API-KEY', 'resource': scanid }
     response = requests.get(scanurl, params=params)
     dictreport=response.json()
     print('Total Scanning >> ',dictreport['total'])
